13c79d4b437445a7296216c686f6088448232ace
Oleksandra Antonova <werba2010@gmail.com>
2023-08-16 21:13:31 -0400

    test

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 const apiKey = document.getElementById('app').getAttribute('data-api-key');
-
-const lat = 123; 
-const lon = 456; 
-
+const lat = 123;
+const lon = 456;
 const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-
 console.log(url);
