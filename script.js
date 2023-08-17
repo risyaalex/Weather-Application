cf49f2d69f3e869c451134c18e8e7d9c5b343499
Oleksandra Antonova <werba2010@gmail.com>
2023-08-16 23:32:19 -0400

    adding functions

--- script.js
+++ script.js
@@ -1,6 +1,68 @@
-const apiKey = process.env.REACT_APP_API_KEY;
-const lat = 123;
-const lon = 456;
-const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-console.log("test ");
-console.log(url);
+const apiKey = "72041d4067825c55c76d14bc52f6fd85";
+const cityInput = document.getElementById('city-input');
+const searchButton = document.getElementById('search-button');
+const weatherInfo = document.getElementById('weather-info');
+let data = null;
+let isLoading = false;
+
+ async function fetchData(city) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    try {
+    isLoading = true;
+    const response = await fetch(url);
+        if (!response.ok) {
+        data = null;
+        throw new Error("Network response was not ok"); 
+    }
+    
+    data = await response.json();
+    console.log("Loading data...");
+    fetchWeatherData(data);
+    console.log("FETCHING DATA...");
+  } catch (error) {
+    console.error(
+      "There was a problem with the fetch operation:",
+      error.message
+    );
+  } finally {
+    isLoading = false;
+    fetchWeatherData(data);
+    console.log("fetchData:", data);
+    console.log("END OF FETCHING DATA...");
+  }
+}
+
+function fetchWeatherData(data) {
+  let content = "";
+
+  if (data) {
+    content = `
+      <h2>${data.name}, ${data.sys && data.sys.country}</h2>
+      <p><strong>Temperature:</strong> ${Math.round(data.main.temp)} °C</p>
+      <p><strong>Feels Like:</strong> ${Math.round(data.main.feels_like)} °C</p>
+      <p><strong>Humidity:</strong> ${data.main.humidity} %</p>
+      <p><strong>Pressure:</strong> ${data.main.pressure} hPa</p>
+      <p><strong>Wind Speed:</strong> ${data.wind.speed} m/s</p>
+    `;
+  } else {
+    content = "<p>City not found or data unavailable...</p>";
+  }
+
+  console.log("fetchWeatherData:", data);
+
+  weatherInfo.innerHTML = isLoading ? "<p>Loading Data...</p>" : content;
+}
+
+
+function searchCity() {
+  const cityName = cityInput.value.trim();
+
+  if (cityName) {
+    fetchData(cityName);
+    }
+  else {
+      weatherInfo.innerHTML = "Please enter city";
+    }
+}
+
+searchButton.addEventListener('click', searchCity);
